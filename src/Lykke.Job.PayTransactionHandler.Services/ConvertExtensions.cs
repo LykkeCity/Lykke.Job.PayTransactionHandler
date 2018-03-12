8408c7e6ed84e0da5cefe82da3b5aa0fb7917801
tarurar <tarutin_andrew@mail.ru>
2018-03-12 21:03:24 +0300

    Merge branch 'feature_LPDEV-238-new-task-for-transfers-controller-instead-of-152'
    Conflicts:
    	src/Lykke.Service.PayInternal/Lykke.Service.PayInternal.csproj

--- src/Lykke.Job.PayTransactionHandler.Services/ConvertExtensions.cs
+++ src/Lykke.Job.PayTransactionHandler.Services/ConvertExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Lykke.Job.PayTransactionHandler.Core.Domain.Common;
 using Lykke.Job.PayTransactionHandler.Core.Domain.TransactionStateCache;
 using Lykke.Job.PayTransactionHandler.Core.Domain.WalletsStateCache;

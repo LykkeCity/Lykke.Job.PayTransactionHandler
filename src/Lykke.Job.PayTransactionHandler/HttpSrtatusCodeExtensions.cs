2841480d929308cc5eb52243131a581cc2218b79
tarurar <tarutin_andrew@mail.ru>
2018-05-24 18:03:14 +0300

    retrying only not 4xx errors

---
+++ src/Lykke.Job.PayTransactionHandler/HttpSrtatusCodeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Lykke.Job.PayTransactionHandler
+{
+    public static class HttpSrtatusCodeExtensions
+    {
+        public static bool Is4xx(this HttpStatusCode statusCode)
+        {
+            return (int) statusCode >= 400 && (int) statusCode < 500;
+        }
+    }
+}
